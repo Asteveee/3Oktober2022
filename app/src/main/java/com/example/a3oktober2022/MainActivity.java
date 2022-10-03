cdd3709c80ae41b09359d4bd25fc821b26257531
Asteveee <114042520+Asteveee@users.noreply.github.com>
2022-10-03 10:45:27 +0700

    CommitPertama

--- app/src/main/java/com/example/a3oktober2022/MainActivity.java
+++ app/src/main/java/com/example/a3oktober2022/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.a3oktober2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnPindah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPindah = findViewById(R.id.btn_pindah);
+
+        btnPindah.setOnClickListener();
+        Override
+                public void onClick(View v) {
+            Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(pindah);
+        }
     }
 }
