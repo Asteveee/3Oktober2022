d5a2c6e05096474da0469a9de927511fcae51dbf
Asteveee <114042520+Asteveee@users.noreply.github.com>
2022-10-03 11:22:41 +0700

    CommitLagi

--- app/src/main/java/com/example/a3oktober2022/MainActivity.java
+++ app/src/main/java/com/example/a3oktober2022/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btnPindah;
@@ -14,13 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnPindah = findViewById(R.id.btn_pindah);
-
-        btnPindah.setOnClickListener();
-        Override
-                public void onClick(View v) {
-            Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
-            startActivity(pindah);
+        public class MainActivity extends AppCompatActivity {
+            Button btnPindah;
+            EditText etNama;
         }
+
+        getSupportActionBar().setTitle("Layout A");
+
+        btnPindah = findViewById(R.id.btn_pindah);
+        etNama = findViewById((R.id.et_nama))
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindahlayout = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(pindahlayout);
+            }
+        });
     }
 }
